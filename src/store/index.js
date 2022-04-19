7569c2e99f1b777511353b42520dec78d19a32a6
Salma EL HAJRAOUI <salma.hajrawi@gmail.com>
2022-04-19 14:53:06 +0200

    create store plugin and use store.subscribe store instance methods

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
-import { createStore } from 'vuex';
-import defaultWorkspace from '@/data/default-workspace';
+import { createStore } from 'vuex'
+import defaultWorkspace from '@/data/default-workspace'
+import { saveStatePlugin } from '@/helpers/utils'
+
+const workspace = JSON.parse(localStorage.getItem('workspace')) || defaultWorkspace
 
 export default createStore({
+  plugins: [saveStatePlugin],
   state: {
-    workspace: defaultWorkspace
+    workspace: workspace
   },
   getters: {
     getWorkspace(state) {

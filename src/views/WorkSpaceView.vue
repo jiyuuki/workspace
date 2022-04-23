6453182c34b1914319d2ef55224a6fe2d4334851
jiyuuki <salma.hajrawi@gmail.com>
2022-04-23 20:09:42 +0200

    clear

--- src/views/WorkSpaceView.vue
+++ src/views/WorkSpaceView.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="workspace">
-    <!-- <div class="flex flex-cols items-start">
-      <div>flex flex-cols</div>
-      <div>flex flex-cols</div>
-      <div>flex flex-cols</div>
-    </div>
-    <div class="flex flex-row items-start">
-      <div>flex flex-row</div>
-      <div>flex flex-row</div>
-      <div>flex flex-row</div>
-    </div>
-    <div class="flex flex-wrap items-start">
-      <div>flex flex-wrap</div>
-      <div>flex flex-wrap</div>
-      <div>flex flex-wrap</div>
-    </div> -->
     <div class="flex flex-row items-start">
       <div
         v-for="(column, $columnIndex) of workspace.columns"

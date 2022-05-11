eabf6387b9d9d61d40580d7987bfc5b5059f052d
jiyuuki <salma.hajrawi@gmail.com>
2022-05-11 19:20:00 +0200

    create drop and drag compenents

---
+++ src/components/VueDrag.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    draggable="true"
+    @dragstart.self="onDrag($event)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueDrag',
+
+  props: {
+    dataTransfer: {
+      type: Object,
+      require: true,
+      default: () => {}
+    }
+  },
+
+  setup(props) {
+    const onDrag = (event) => {
+      event.dataTransfer.setData('payload', JSON.stringify(props.dataTransfer))
+    }
+
+    return {
+      onDrag,
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/components/VueDrop.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    @drop.self="onDrop($event)"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VueDrop',
+
+  emits: ['drop'],
+
+  setup(props, { emit }) {
+    const onDrop = (event) => {
+      const dataTransfer = JSON.parse(event.dataTransfer.getData('payload'))
+      emit('drop', dataTransfer)
+    }
+
+    return {
+      onDrop,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
